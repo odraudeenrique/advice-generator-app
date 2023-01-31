f00983002c23f183b8cfa8bcb807fa6113546546
Eduardo Rodriguez <eduardoenriquerodriguez1991@gmail.com>
2023-01-31 19:31:53 -0300

    third commit

--- adviceGenerator.js
+++ adviceGenerator.js
@@ -1,24 +1,42 @@
-const adviceNumber=document.querySelector("#advice-number-span")
-const advice=document.querySelector("#advice-span")
-const dice=document.querySelector(".dice")
+const adviceNumber=document.querySelector("#advice-number-span");
+const advice=document.querySelector("#advice-span");
+const dice=document.querySelector(".dice");
 const url = "https://api.adviceslip.com/advice";
-const wholeDimension=document.getElementById('whole-section-part')
-const cardDimension=document.getElementById('advice-card-part')
-const cardColor=document.querySelectorAll('.advice-card')
+const wholeDimension=document.getElementById('whole-section-part');
+const cardDimension=document.getElementById('advice-card-part');
+const cardColor=document.querySelectorAll('.advice-card');
 
 let widthDimensions
 let heightDimensions
 
 function displayAdvice(response){
-    adviceNumber.innerHTML=response.data.slip.id 
-    advice.innerHTML=response.data.slip.advice   
+    adviceNumber.innerHTML=response.data.slip.id;
+    advice.innerHTML=response.data.slip.advice;  
 }
+
 function getAdvice(params) {
     axios.get(url)
         .then(displayAdvice)
+        .then(()=>{
+            dice.classList.remove("loader-animation");
+        })
 }
-getAdvice()
-dice.addEventListener('click',getAdvice)
+
+function actionButton(){
+    dice.classList.add("loader-animation");
+    getAdvice();
+}
+
+getAdvice();
+dice.addEventListener('click',actionButton);
+
+widthDimensions=window.innerWidth-40;
+heightDimensions=window.innerHeigh-40; 
+
+cardDimension.width=widthDimensions;
+
+
+
 
 
 /*function changeColor(){
@@ -32,7 +50,4 @@ cardDimension.addEventListener('mouseout', (e)=>{
     e.target.style.backgroundColor='#FF4949';
 }
 )*/
-widthDimensions=window.innerWidth-40
-heightDimensions=window.innerHeigh-40 
 
-cardDimension.width=widthDimensions
